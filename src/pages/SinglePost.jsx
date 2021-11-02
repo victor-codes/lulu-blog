91753c03a52cc7f92c22daa66ae20a0ea1d6319e
Adedapo Ayodeji <ayodejiv5@gmail.com>
2021-11-02 01:29:10 +0100

    Feat: working on blog post

--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -1,17 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { ReactComponent as ArrowRight } from "../assets/arrow-right.svg";
 import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
 import Image from "../assets/post-image.jpg";
 import Footer from "../components/Footer";
 import { Helmet } from "react-helmet";
-import content from "../components/Content.js";
-import ReactMarkdown from "react-markdown";
+import Markdown from "react-markdown";
+import postData from "../data/posts.json";
+import hljs from "highlight.js";
+import python from "highlight.js/lib/languages/python";
+import css from "highlight.js/lib/languages/css";
+import xml from "highlight.js/lib/languages/xml";
 
 const SinglePost = () => {
+  const codeRef = useRef();
   const { slug } = useParams();
   const titleSlug = slug.split("-").join(" ");
   const title = titleSlug.charAt(0).toUpperCase() + titleSlug.slice(1);
+  hljs.registerLanguage("python", python);
 
   const [scroll, setScroll] = useState(0);
 
@@ -26,7 +32,20 @@ const SinglePost = () => {
 
     return window.scrollTo(0, scroll);
   }
-console.log(content);
+
+  hljs.registerLanguage("css", css);
+  hljs.registerLanguage("xml", xml);
+  let fetchedData = postData[0];
+
+  let content = fetchedData.content;
+
+  useEffect(() => {
+    const nodes = codeRef.current.querySelectorAll("pre");
+    nodes.forEach((node) => {
+      hljs.highlightBlock(node);
+    });
+  }, [codeRef]);
+
   return (
     <div className="single_post_footer_bg">
       <Helmet>
@@ -51,10 +70,9 @@ console.log(content);
             <img src={Image} alt="" />
             <figcaption>Image alt text</figcaption>
           </figure>
-          <div className="post_content">
+          <article ref={codeRef} className="post_content">
+            <Markdown source={content} escapeHtml={true} />
 
-          <ReactMarkdown source={content} />
-            
             {/* <p>
               To design user-oriented products, it’s important that you carry
               out research. User Research is a method used to understand the
@@ -75,7 +93,7 @@ console.log(content);
               products, it’s important that you carry out research. User
               Research is a method used to understand the behaviors.
             </p> */}
-          </div>
+          </article>
           <div className="share_link">
             <div>
               <p>SHARE TO</p>
