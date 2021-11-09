02d01807cf7d43d7b08c78b88bea1188bba95021
Adedapo Ayodeji <ayodejiv5@gmail.com>
2021-11-09 12:21:05 +0100

    Fix: metadata

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import { truncateString } from "../utils/helperFunction";
 
 const Title = ({ title, description, imageUrl }) => {
+  const defaultImgUrl = "";
+  const defaultDescriptionText = "";
+  const trimmedDescription = truncateString(description, 150);
 
   return (
     <Helmet>
       {/* // <!-- Primary Meta Tags --> */}
       <title>{`${title} — Lulu Nwenyi`}</title>
       <meta name="title" content={`${title} — Lulu Nwenyi`} />
-      <meta name="description" content={`${description}`} />
+      <meta
+        name="description"
+        content={`${
+          description ? trimmedDescription : defaultDescriptionText
+        } Read more...`}
+      />
 
       {/* <!-- Open Graph / Facebook --> */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content={`${window.location.href}`} />
       <meta property="og:title" content={`${title} — Lulu Nwenyi`} />
-      <meta property="og:description" content={`${description}`} />
+      <meta
+        property="og:description"
+        content={`${
+          description ? trimmedDescription : defaultDescriptionText
+        } Read more...`}
+      />
       <meta
         property="og:image"
         content={`${
@@ -26,7 +40,12 @@ const Title = ({ title, description, imageUrl }) => {
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={`${window.location.href}`} />
       <meta property="twitter:title" content={`${title} — Lulu Nwenyi`} />
-      <meta property="twitter:description" content={`${description}`} />
+      <meta
+        property="twitter:description"
+        content={`${
+          description ? trimmedDescription : defaultDescriptionText
+        } Read more...`}
+      />
       <meta
         property="twitter:image"
         content={`${
