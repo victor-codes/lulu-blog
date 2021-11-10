864d22973f5324ba3f9234d0e3307639a1123145
Adedapo Ayodeji <ayodejiv5@gmail.com>
2021-11-10 18:40:43 +0100

    remove animation

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,35 +1,32 @@
-import { motion } from "framer-motion";
 import React from "react";
 import { Link, useHistory } from "react-router-dom";
 import { ReactComponent as Arrow } from "../assets/arrow-right.svg";
 import { stringToLink, truncateString } from "../utils/helperFunction";
+// import { motion } from "framer-motion;
+
 const Post = ({ post }) => {
   const { tags, title, publishDate, description, category } = post;
+  // const link = {
+  //   visible: { opacity: 1, y: -4 },
+  //   hidden: { opacity: 0, y: 0 },
+  // };
 
-  const link = {
-    visible: { opacity: 1, y: -4 },
-    hidden: { opacity: 0, y: 0 },
-  };
+  // const item = {
+  //   visible: {
+  //     opacity: 1,
+  //     x: 8,
+  //   },
+  //   hidden: {
+  //     opacity: 0,
+  //     x: 8,
+  //   },
+  // };
 
-  const item = {
-    visible: {
-      opacity: 1,
-      x: 8,
-    },
-    hidden: {
-      opacity: 0,
-      x: 8,
-    },
-  };
-
-  const history = useHistory()
+  const history = useHistory();
 
   return (
-    <motion.article
-      whileHover="visible"
-      variants={link}
+    <article
       className="post_article"
-      transition={{ type: "spring", duration: 0.03 }}
       onClick={() => {history.push(`/${stringToLink(category)}/post/${stringToLink(title)}`);}}
     >
       <div className="info">
@@ -40,16 +37,14 @@ const Post = ({ post }) => {
         <span className="timestamp">{publishDate}</span>
       </div>
       <p>{truncateString(description, 150)}</p>
-      <motion.div>
-        <Link to={`/${stringToLink(category)}/post/${stringToLink(title)}`}>
-          READ MORE
-          <motion.div transition={{ duration: 0.03 }} variants={item}>
-            <Arrow />
-          </motion.div>
-        </Link>
-      </motion.div>
+
+      <Link to={`/${stringToLink(category)}/post/${stringToLink(title)}`}>
+        READ MORE
+        <Arrow />
+      </Link>
+
       <div className="line_divider"></div>
-    </motion.article>
+    </article>
   );
 };
 
