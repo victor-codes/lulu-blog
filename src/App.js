9a07a42e2830825f9394d3bb73c3076dbd94c74c
Adedapo Ayodeji <ayodejiv5@gmail.com>
2021-11-15 08:53:28 +0100

    remove animations

--- src/App.js
+++ src/App.js
@@ -23,69 +23,71 @@ function App() {
       <Router>
         <CategoryProvider>
           <Header />
-          <Route
-            render={({ location }) => (
-              <AnimatePresence exitBeforeEnter initial={false}>
-                <Switch location={location} key={location.pathname}>
-                  <Route exact path="/" component={Home} key={0} />
+          {/* <Route
+            render={({ location }) => ( */}
+          <AnimatePresence exitBeforeEnter initial={false}>
+            <Switch
+            // location={location} key={location.pathname}
+            >
+              <Route exact path="/" component={Home} key={0} />
 
-                  {/* categories */}
-                  <Route exact path="/design">
-                    <Redirect to="/design/all" />
-                  </Route>
-                  <Route exact path="/design/:subcategory" key={1}>
-                    <Category name="Design" />
-                  </Route>
+              {/* categories */}
+              <Route exact path="/design">
+                <Redirect to="/design/all" />
+              </Route>
+              <Route exact path="/design/:subcategory" key={1}>
+                <Category name="Design" />
+              </Route>
 
-                  <Route exact path="/dev">
-                    <Redirect to="/dev/all" />
-                  </Route>
+              <Route exact path="/dev">
+                <Redirect to="/dev/all" />
+              </Route>
 
-                  <Route exact path="/dev/:subcategory">
-                    <Category name="Dev" key={2} />
-                  </Route>
+              <Route exact path="/dev/:subcategory">
+                <Category name="Dev" key={2} />
+              </Route>
 
-                  <Route exact path="/cloud">
-                    <Redirect to="/cloud/all" />
-                  </Route>
+              <Route exact path="/cloud">
+                <Redirect to="/cloud/all" />
+              </Route>
 
-                  <Route exact path="/cloud/devops">
-                    <Redirect to="/cloud/all" />
-                  </Route>
+              <Route exact path="/cloud/devops">
+                <Redirect to="/cloud/all" />
+              </Route>
 
-                  <Route exact path="/cloud/:subcategory">
-                    <Category name="DevOps" key={3} />
-                  </Route>
+              <Route exact path="/cloud/:subcategory">
+                <Category name="DevOps" key={3} />
+              </Route>
 
-                  <Route exact path="/psych">
-                    <Redirect to="/psych/all" />
-                  </Route>
-                  <Route exact path="/psych/:subcategory">
-                    <Category name="Psych" key={4} />
-                  </Route>
+              <Route exact path="/psych">
+                <Redirect to="/psych/all" />
+              </Route>
+              <Route exact path="/psych/:subcategory">
+                <Category name="Psych" key={4} />
+              </Route>
 
-                  <Route exact path="/resources">
-                    <Redirect to="/resources/all" />
-                  </Route>
-                  <Route exact path="/resources/:subcategory" key={5}>
-                    <Category name="Resources" />
-                  </Route>
-                  <Route exact path="/sponsor" component={Home} key={6} />
+              <Route exact path="/resources">
+                <Redirect to="/resources/all" />
+              </Route>
+              <Route exact path="/resources/:subcategory" key={5}>
+                <Category name="Resources" />
+              </Route>
+              <Route exact path="/sponsor" component={Home} key={6} />
 
-                  <Route
-                    exact
-                    strict
-                    path="/:category/post/:slug"
-                    component={SinglePost}
-                    key={7}
-                  />
+              <Route
+                exact
+                strict
+                path="/:category/post/:slug"
+                component={SinglePost}
+                key={7}
+              />
 
-                  <Route exact path="*" component={PageNotFound} key={8} />
-                  <Route exact path="/404" component={PageNotFound} key={9} />
-                </Switch>
-              </AnimatePresence>
-            )}
-          />
+              <Route exact path="*" component={PageNotFound} key={8} />
+              <Route exact path="/404" component={PageNotFound} key={9} />
+            </Switch>
+          </AnimatePresence>
+          {/* )}
+          /> */}
         </CategoryProvider>
       </Router>
     </div>
